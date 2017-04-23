cab87da88066c1ffc798047543ac70712ac6cf04
Daniel Garrido <daniel.garrido.info@gmail.com>
2017-04-23 17:16:02 +0100

    test can get song name

--- song.rb
+++ song.rb
@@ -1,3 +1,8 @@
 class Song
 
+  attr_reader :name
+  
+  def initialize(song_name)
+    @name = song_name
+  end
 end
--- specs/song_spec.rb
+++ specs/song_spec.rb
@@ -5,11 +5,15 @@ require_relative("../song.rb")
 class TestSong < MiniTest::Test
 
 def setup
-  @song1 = Song.new
+  @song1 = Song.new("Paranoid")
 end
 
 def test_can_create_song_class
   assert_equal(Song, @song1.class)
+end
+
+def test_can_get_song_name
+  assert_equal("Paranoid", @song1.name)
 end
 
 
