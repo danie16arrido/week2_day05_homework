9792d8053af7b615201b8f1a53b04fee5af635ae
Daniel Garrido <daniel.garrido.info@gmail.com>
2017-04-23 16:30:45 +0100

    create class Room

--- specs/karaoke_specs.rb
+++ specs/karaoke_specs.rb
@@ -6,6 +6,7 @@ class TestKaraoke < MiniTest::Test
 
 def setup
   @karaoke1 = Karaoke.new
+  @room1 = Room.new
 end
 
 def test_can_create_karaoke
@@ -14,6 +15,11 @@ end
 
 def test_can_store_rooms
   assert_equal([], @karaoke1.rooms)
+end
+
+def test_can_add_room
+  @karaoke1.add_room()
+  assert_equal(1, @karaoke1.rooms.count())
 end
 
 
---
+++ specs/room_spec.rb
@@ -0,0 +1,15 @@
+require('minitest/autorun')
+require('minitest/emoji')
+require_relative("../room.rb")
+
+class TestRoom < MiniTest::Test
+
+  def setup
+    @room1 = Room.new
+  end
+
+  def test_can_create_room
+    assert_equal(Room, @room1.class)
+  end
+
+end
