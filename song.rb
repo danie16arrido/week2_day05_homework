481e8954d838ee64c5aba307abd22cbc0d958f31
Daniel Garrido <daniel.garrido.info@gmail.com>
2017-04-23 17:21:19 +0100

    test can get duration of song in seconds

--- song.rb
+++ song.rb
@@ -1,9 +1,10 @@
 class Song
 
-  attr_reader :name, :artist
+  attr_reader :name, :artist, :duration
 
-  def initialize(song_name, artist_name)
+  def initialize(song_name, artist_name, duration)
     @name = song_name
     @artist = artist_name
+    @duration = duration
   end
 end
--- specs/song_spec.rb
+++ specs/song_spec.rb
@@ -5,7 +5,7 @@ require_relative("../song.rb")
 class TestSong < MiniTest::Test
 
 def setup
-  @song1 = Song.new("Paranoid", "Black Sabbath")
+  @song1 = Song.new("Paranoid", "Black Sabbath", 170)
 end
 
 def test_can_create_song_class
@@ -18,6 +18,10 @@ end
 
 def test_can_get_artist
   assert_equal("Black Sabbath", @song1.artist)
+end
+
+def test_can_get_duration
+  assert_equal(170, @song1.duration)
 end
 
 
