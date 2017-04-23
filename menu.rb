987225bb0f3e332bd35b67640ef1bb16331cc5a7
Daniel Garrido <daniel.garrido.info@gmail.com>
2017-04-23 19:13:55 +0100

    test can add menu item either a string or and array

--- menu.rb
+++ menu.rb
@@ -1,8 +1,20 @@
 class Menu
 
-  attr_reader :name
+  attr_reader :name, :items
 
   def initialize(a_name)
     @name = a_name
+    @items = []
+  end
+
+  def add_item(item)
+    if item.class == Array
+      item.each do |item|
+        @items << item
+      end
+    else
+      @items << item
+    end
+
   end
 end
--- specs/menu_spec.rb
+++ specs/menu_spec.rb
@@ -14,4 +14,11 @@ class TestMenu < MiniTest::Test
   def test_can_get_name
     assert_equal("Main", @menu_main.name)
   end
+
+  def test_can_add_menu_items
+
+    @menu_main.add_item("Add song to playlist")
+    @menu_main.add_item(["Buy a beer", "Buy snacks"])
+    assert_equal(3, @menu_main.items.count())
+  end
 end
