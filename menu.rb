6f412364d05568542d9ed0c7dd594a4ba3b34811
Daniel Garrido <daniel.garrido.info@gmail.com>
2017-04-23 19:07:47 +0100

    test can get menu name

--- menu.rb
+++ menu.rb
@@ -1,3 +1,8 @@
 class Menu
 
+  attr_reader :name
+
+  def initialize(a_name)
+    @name = a_name
+  end
 end
--- specs/menu_spec.rb
+++ specs/menu_spec.rb
@@ -4,10 +4,14 @@ require_relative("../menu.rb")
 
 class TestMenu < MiniTest::Test
   def setup 
-    @menu_main = Menu.new
+    @menu_main = Menu.new("Main")
   end
 
   def test_can_create_menu
     assert_equal(Menu, @menu_main.class)
+  end
+
+  def test_can_get_name
+    assert_equal("Main", @menu_main.name)
   end
 end
